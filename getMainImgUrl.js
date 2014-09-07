9b646517b3932fe26864a5278f85222e1c070ea8
tiantianquan <fenqiufen@gmail.com>
2014-09-07 21:38:51 +0800

    commit

--- getMainImgUrl.js
+++ getMainImgUrl.js
@@ -1,8 +1,9 @@
 var scraperjs = require('scraperjs');
-var fs = require('fs');
+var fs = require('graceful-fs');
 var http = require('http');
 var Pokemandb = require('./PokemanSchema.js');
 var under = require('underscore');
+var request = require('request');
 
 var item = 0;
 
@@ -18,18 +19,48 @@ function scrapeImg(pokeman) {
     },function(link){
       pokeman.ImgUrl.MainImg = link;
       pokeman.save(function(err){});
-      http.get(link, function(res){
-        var imgData='';
-        res.setEncoding('binary');
 
-        res.on('data', function(chunk){
-          imgData+=chunk;
+      var file = fs.createWriteStream('./img/mainImg/'+pokeman.No.split('#')[1]+pokeman.EName+'.png');
+      if (typeof(link) != 'string') return;
+        
+      var options = {
+        host: 'static.52poke.com',
+        port: 80,
+        path: link.split('http://static.52poke.com')[1]
+      };
+      var request = http.get(options, function(res) {
+        res.pipe(file);
+        file.on('open',function(){console.log('stream open')})
+        file.on('finish',function(){
+          console.log(link);
+          file.close();
         });
-        res.on('end',function(){
-          fs.writeFileSync('./img/mainImg/'+pokeman.No.split('#')[1]+pokeman.EName+'.png', imgData);
-          console.log(item++);
+        file.on('end',function(){
+          console.log('stream end');
+          file.close();
         });
+        file.on('error',function(e){console.log("Got error: " + e.message);});
+      });
+      request.on('error', function(e) {
+        console.log("Got error: " + e.message);
+      });
 
-      })
+      // request(
+      //   {
+      //     method: 'GET',
+      //     uri: link
+      //   },
+      //   function(err, res, body){
+      //     if(err){
+      //       console.log('Got error:' + err.message);
+      //     }
+      //     else if(res.statusCode == 404){ console.log('404');}
+      //     else {
+      //       res.pipe(file);
+      //       file.on('finish',function(){file.close();});
+      //     }
+      //   }
+
+      // )
     })
   }
